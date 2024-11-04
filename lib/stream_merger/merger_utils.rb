7111cd83c0d0f15072cb5822a717687f27eb91fc
Mauricio Murga <murga.mauricio@gmail.com>
2024-11-03 18:46:36 -0600

    feature: start to implement instructions and start to fix aspect ratio

--- lib/stream_merger/merger_utils.rb
+++ lib/stream_merger/merger_utils.rb
@@ -4,28 +4,28 @@ module StreamMerger
   # MergeUtils
   module MergerUtils
     ONE_GRID = "[0:v]scale=1080:1920[video]"
-    TWO_GRID = "[0:v]scale=1080:960[top]; \
-         [1:v]scale=1080:960[bottom]; \
+    TWO_GRID = "[0:v]scale=540:-1[top]; \
+         [1:v]scale=540:-1[bottom]; \
          [top][bottom]vstack=inputs=2:shortest=1[video]; \
          [0:a][1:a]amix=inputs=2:duration=shortest:dropout_transition=3[audio]"
-    THREE_GRID = "[0:v]scale=1080:960[top]; \
-         [1:v]scale=540:360[bottom_left]; \
-         [2:v]scale=540:360[bottom_right]; \
+    THREE_GRID = "[0:v]scale=-1:960[top]; \
+         [1:v]scale=540:480[bottom_left]; \
+         [2:v]scale=540:480[bottom_right]; \
          [bottom_left][bottom_right]hstack=inputs=2[bottom]; \
          [top][bottom]vstack=inputs=2:shortest=1[video]; \
          [0:a][1:a][2:a]amix=inputs=3:duration=shortest:dropout_transition=3[audio]"
-    FOUR_GRID = "[0:v]scale=540:960[top_left]; \
-         [1:v]scale=540:960[top_right]; \
-         [2:v]scale=540:360[bottom_left]; \
-         [3:v]scale=540:360[bottom_right]; \
+    FOUR_GRID = "[0:v]scale=540:-1[top_left]; \
+         [1:v]scale=540:-1[top_right]; \
+         [2:v]scale=540:-1[bottom_left]; \
+         [3:v]scale=540:-1[bottom_right]; \
          [top_left][top_right]hstack=inputs=2[top]; \
          [bottom_left][bottom_right]hstack=inputs=2[bottom]; \
          [top][bottom]vstack=inputs=2:shortest=1[video]; \
          [0:a][1:a][2:a][3:a]amix=inputs=4:duration=shortest:dropout_transition=3[audio]"
     GRIDS = [ONE_GRID, TWO_GRID, THREE_GRID, FOUR_GRID].freeze
 
-    def merge_streams(streams)
-      cmd = base_ffmpeg_command(inputs(streams), grid(streams))
+    def merge_streams(instructions)
+      cmd = base_ffmpeg_command(inputs(instructions), grid(instructions))
       run_ffmpeg(cmd)
     end
 
@@ -42,7 +42,25 @@ module StreamMerger
       CMD
     end
 
-    def inputs(streams)
+    # def inputs(instructions)
+    #   streams = instructions.map.with_index do |instruction, index|
+    #     file = instruction[:file]
+    #     start = instruction[:start_seconds]
+    #     end_time = instruction[:end_seconds]
+
+    #     # Constructing the input command with trimming filters
+    #     input_command = "-i \"#{file}\""
+    #     trim_video = "[#{index}:v]trim=0.747:32.689077,setpts=PTS-STARTPTS[top];"
+    #     trim_audio = "[#{index}:a]atrim=0.747:32.689077[audio#{index}];"
+
+    #     "#{input_command} #{trim_video} #{trim_audio}"
+    #   end
+
+    #   streams.join(" ")
+    # end
+
+    def inputs(instructions)
+      streams = instructions.map { |instruction| instruction[:file] }
       streams.map do |stream|
         "-i \"#{stream}\""
       end.join(" ")
--- spec/stream_merger/merger_utils_spec.rb
+++ spec/stream_merger/merger_utils_spec.rb
@@ -2,10 +2,14 @@
 
 RSpec.describe StreamMerger::MergerUtils do
   let(:dummy_class) { Class.new { extend StreamMerger::MergerUtils } }
-  let(:manifest1) { file_path("ewbmlXE8Py7L-2024-11-01_19-51-01.198.m3u8") }
-  let(:manifest2) { file_path("ZqueuFbL1FQj-2024-11-01_19-51-01.945.m3u8") }
+  let(:instructions) do
+    [{ file: file_path("ewbmlXE8Py7L-2024-11-01_19-51-01.198.m3u8"), start_seconds: 0.747,
+       end_seconds: 32.689077000000005 },
+     { file: file_path("ZqueuFbL1FQj-2024-11-01_19-51-01.945.m3u8"), start_seconds: 0, end_seconds: 31.942077 }, { file: file_path("ewbmlXE8Py7L-2024-11-01_19-51-01.198.m3u8"), start_seconds: 0.747,
+                                                                                                                   end_seconds: 32.689077000000005 }]
+  end
 
   it "foo" do
-    dummy_class.merge_streams([manifest1, manifest2])
+    dummy_class.merge_streams(instructions)
   end
 end
