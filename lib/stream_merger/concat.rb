5894db7ecda0fdf0858dd50902ff1bc05a6ce555
Mauricio Murga <murga.mauricio@gmail.com>
2024-11-12 15:51:19 -0600

    progress: Upload to s3 is broken through AMS, will try another way

--- lib/stream_merger/concat.rb
+++ lib/stream_merger/concat.rb
@@ -17,7 +17,7 @@ module StreamMerger
         ffmpeg -y -safe 0 -i #{@concat_pls} \
         -preset ultrafast -pix_fmt yuv420p -r 30 -g 30 -c:v libx264 -c:a aac -f hls \
         -hls_time 1 -hls_list_size 0 -hls_flags append_list \
-        -hls_segment_filename '#{@conference_id}_%03d.ts' "#{@conference_id}.m3u8")}'
+        -hls_segment_filename './tmp/#{@conference_id}_%03d.ts' './tmp/#{@conference_id}.m3u8'
       CMD
 
       @ffmpeg_process ||= IO.popen(cmd, "w")
